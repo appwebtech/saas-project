864014498de97b513121c0bd34c9dfe353e2dbe5
Joseph <josembi@gmail.com>
2018-05-25 16:06:00 +0100

    Validations and Uploadable File size Restritions

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,3 +1,21 @@
-class Artifact < ApplicationRecord
+
+class Artifact < ActiveRecord::Base
+  
+  before_save :upload_to_s3
+  attr_accessor :upload
   belongs_to :project
+  
+  MAX_FILESIZE = 10.megabytes
+  validates_presence_of :name, :upload
+  validates_uniqueness_of :name
+  
+  validate :uploaded_fize_size
+  
+  private
+
+  def uploaded_fize_size
+    if upload 
+      errors.add(:upload, "File size must be less than #{self.class::MAX_FILESIZE}") unless upload.size <= self.class::MAX_FILESIZE
+    end
+  end
 end
